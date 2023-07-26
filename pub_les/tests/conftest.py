685b91e0864cf269651bcc6aa494df759b813d31
ZemAleks <xandrzem@yandex.ru>
2023-07-26 23:36:35 +0400

    added fixtures to the file conftest.py

--- pub_les/tests/conftest.py
+++ pub_les/tests/conftest.py
@@ -1,25 +1,17 @@
 import pytest
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def rest_service():
     print("Setting up REST service - Настройка REST-сервиса")
-    a = "code"
-    b = 200
-    yield a, b
+    yield
     print("Shutting down REST service - Отключение службы REST")
-
-
-@pytest.fixture
-def say_hello(rest_service):
-    a1, b1 = rest_service  # получение адрессов из фикстуры
-    print("hello")
-    return rest_service
 
 
 @pytest.fixture()
 def connect_to_database():
     connection = "- Connection process started - code status 200"
+    return connection
 
 
 @pytest.fixture()
--- pub_les/tests/test_sample.py
+++ pub_les/tests/test_sample.py
@@ -1,20 +1,8 @@
 def test_one(rest_service):
-    a1, b1 = rest_service  # получение адрессов из фикстуры
-    result = "Service setup test data - Данные тестовой настройки"
+    result = "service setup test data"
     assert result
-    assert a1
-    assert b1
-    assert rest_service[0]  # взять первый элемент
     print(result)
-    print(rest_service)
-    print(rest_service[0])
-    print(a1, b1)
-
-
-def test_another():
-    answer = 100 + 1
-    assert answer
-    print(answer)
+    # print(rest_service) # результат, который вернул тест
 
 
 def test_two(database):
@@ -23,15 +11,10 @@ def test_two(database):
     print("result = ", summ, "sec")
 
 
-def test_three(say_hello):
-    answer = "validation response = something - твет проверки = что-нибудь"
-    assert answer
-    assert say_hello
-    print(answer)
-    print(say_hello)
-    print("server response result, = ", say_hello, "код результат ответа сервера")
+def test_three():
+    assert "something"
 
 
 def test_connect(connect_to_database):  # Прокидываем фикстуру в тест и она выполнится перед тестом
     print(connect_to_database)  # Используем фикстуру, она вернет нам текст с подключением к БД
-    print("Database connection established - Соединение с базой данных установлено")
+    print("Database connection established")
