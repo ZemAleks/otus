6535ed6b099cbea86433d681325aa3abfa6424f5
ZemAleks <xandrzem@yandex.ru>
2023-07-30 19:32:29 +0400

    adding fixture extensions

---
+++ pub_les/tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+
+
+@pytest.fixture(scope="function")
+def rest_service():
+    print("Setting up REST service - Настройка REST-сервиса")
+    yield
+    print("Shutting down REST service - Отключение службы REST")
+
+
+@pytest.fixture()
+def connect_to_database():
+    connection = "- Connection process started - code status 200"
+    return connection
+
+
+@pytest.fixture()
+def database():
+    print("Setting up database - Настройка базы данных")
+    yield
+    print("Shutting down database - Выключение базы данных")
--- pub_les/tests/test_sample.py
+++ pub_les/tests/test_sample.py
@@ -1,10 +1,20 @@
-def test_one():
-    pass
+def test_one(rest_service):
+    result = "service setup test data"
+    assert result
+    print(result)
+    # print(rest_service) # результат, который вернул тест
 
 
-def test_two():
-    assert 1 + 1 == 2
+def test_two(database):
+    x, y, summ = 1, 2, 3
+    assert x + y == summ
+    print("result = ", summ, "sec")
 
 
 def test_three():
     assert "something"
+
+
+def test_connect(connect_to_database):  # Прокидываем фикстуру в тест и она выполнится перед тестом
+    print(connect_to_database)  # Используем фикстуру, она вернет нам текст с подключением к БД
+    print("Database connection established")
