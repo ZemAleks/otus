24777e13c41091d1cc41a3085aa7d361addddf3b
ZemAleks <xandrzem@yandex.ru>
2023-11-30 01:22:50 +0400

    Обновление тестов

--- pub_les/tests/conftest.py
+++ pub_les/tests/conftest.py
@@ -1,29 +1,25 @@
 import pytest
 
 
-<<<<<<< HEAD
-@pytest.fixture(scope="function")
-=======
-@pytest.fixture()
->>>>>>> 3c779fa (added fixtures to the file conftest.py)
+@pytest.fixture(scope="module", autouse=True)
 def rest_service():
-    print("Setting up REST service - Настройка REST-сервиса")
+    print("Setting up REST service - Настройка REST-сервиса")   # инициализация и поднятие сервисов
     yield
     print("Shutting down REST service - Отключение службы REST")
 
 
 @pytest.fixture()
 def connect_to_database():
-<<<<<<< HEAD
-    connection = "- Connection process started - code status 200"
-=======
-    connection = "- Connection process started -"
->>>>>>> 3c779fa (added fixtures to the file conftest.py)
-    return connection
+    connection = "Code status 200 - connection successful - Статус кода 200 – соединение успешное."
+    return print(connection)
 
 
-@pytest.fixture()
+@pytest.fixture(scope="module", autouse=True)
 def database():
     print("Setting up database - Настройка базы данных")
     yield
-    print("Shutting down database - Выключение базы данных")
+    print("Shutting down database - Отключение базы данных")
+
+
+
+
