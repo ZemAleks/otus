68e7debe404648891f65b36ae47789b542349279
ZemAleks <xandrzem@yandex.ru>
2023-12-10 19:51:06 +0400

    дополнил тесты и добавил исключения

--- hw3_oop/tests/test_classes.py
+++ hw3_oop/tests/test_classes.py
@@ -3,8 +3,11 @@ from hw3_oop.src.Square import Square
 from hw3_oop.src.Triangle import Triangle
 from hw3_oop.src.Circle import Circle
 import pytest
+from contextlib import nullcontext as does_not_raise
 
 """
+- Используем the Task type для отображения сбоев тестов.
+-
 - Тесты по настройки системы.
 - Запускают конфигурационные файлы для настройки соединения с шиной.
 - Запускают процессы CI разворачивают контейнеры и устанавливают тестовую среду.
@@ -23,13 +26,9 @@ def test_running():
     print(result)  # результат, который вернул тест
 
 
-"""Конфигурационные тесты по настройке и подключению к базе данных"""
-
-
-def test_rectangle_db(get_rectangle_from_db):
-    side_a, side_b, side_c, side_d = get_rectangle_from_db
-    r = Rectangle(side_a, side_b, side_c, side_d)
-    assert r.get_area() == side_a * side_b
+"""
+- Конфигурационные тесты по настройке и подключению к базе данных.
+"""
 
 
 @pytest.mark.parametrize(("rectangle_sides", "area", "perimeter"),
@@ -59,9 +58,12 @@ def test_status_db():
     print(status_down)
 
 
-"""Тесты проверки проектного модуля системы создания Геометрических фигур."""
+"""
+- Тесты проверки проектного модуля системы создания Геометрических фигур.
+"""
 
 
+# проверка расчета параметров прямоугольника
 @pytest.mark.parametrize(("side_a", "side_b", "side_c", "side_d", "area", "perimeter"),
                          [(4, 6, 4, 6, 24, 20),
                           (5, 10, 5, 10, 50, 30)])
@@ -72,18 +74,20 @@ def test_rectangle(side_a: int, side_b: int, side_c: int, side_d: int, area: flo
     assert r.get_perimeter() == perimeter
 
 
-@pytest.mark.parametrize(("side_a", "side_b", "side_c", "side_d", "area", "perimeter"),
-                         [(-4, -6, 4, 6, 24, -20),
-                          (0, 0, 0, 0, 0, 0),
-                          (5, 10, 5, 10, 50, 30)])
-def test_rectangle_negative(side_a: int, side_b: int, side_c: int, side_d: int, area: float, perimeter: float):
-    with pytest.raises(ValueError):
+# проверка исключений расчета параметров прямоугольника
+@pytest.mark.parametrize(("side_a", "side_b", "side_c", "side_d", "area", "perimeter", "expectation"),
+                         {(-4, -6, 4, 6, 24, -20, pytest.raises(ValueError)),  # вызовет исключения
+                          (0, 0, 0, 0, 0, 0, pytest.raises(ValueError)),  # вызовет исключения
+                          (5, 10, 5, 10, 50, 30, does_not_raise())})  # не вызовет исключения
+def test_rectangle_negative(side_a: int, side_b: int, side_c: int, side_d: int, area: float, perimeter: float, expectation):
+    with expectation:
         r = Rectangle(side_a, side_b, side_c, side_d)
         assert r.name == f"Rectangle"
         assert r.get_area() == area
         assert r.get_perimeter() == perimeter
 
 
+# проверка расчета параметров квадрата
 @pytest.mark.parametrize(("side_a", "side_b", "side_c", "side_d", "area", "perimeter"),
                          [(4, 4, 4, 4, 16, 16),
                           (5, 5, 5, 5, 25, 20),
@@ -95,6 +99,7 @@ def test_square(side_a: int, side_b: int, side_c: int, side_d: int, area: float,
     assert r.get_perimeter() == perimeter
 
 
+# проверка расчета параметров треугольника
 @pytest.mark.parametrize(("side_a", "side_b", "side_c", "area", "perimeter"),
                          [(3, 5, 7, 6.49519052838329, 15),
                           (4, 3, 6, 5.332682251925386, 13),
@@ -106,6 +111,7 @@ def test_triangle(side_a: int, side_b: int, side_c: int, area: float, perimeter:
     assert r.get_perimeter() == perimeter
 
 
+# проверка расчета параметров окружности
 @pytest.mark.parametrize(("axis_x", "axis_y", "radius", "area", "perimeter"),
                          [(0, 0, 1, 3.14, 6.28),
                           (0, 0, 2, 12.56, 12.56),
@@ -117,20 +123,32 @@ def test_circle(axis_x: int, axis_y: int, radius: int, area: float, perimeter: f
     assert r.get_perimeter() == perimeter
 
 
+"""
+- Тесты проверки расчета сумму площадей этих Геометрических фигур.
+"""
+
+
+# проверка расчета сумму площадей прямоугольника и квадрата
 def test_add_area():
-    r = Rectangle(2, 5, 2, 5)  # 10
-    s = Square(5, 5, 5, 5)  # 25
+    r = Rectangle(2, 5, 2, 5)  # площадь прямоугольника 10
+    s = Square(5, 5, 5, 5)  # площадь квадрата 25
     assert r.add_area(s) == (10 + 25)
 
 
+# проверка расчета сумму площадей прямоугольника и окружности
 def test_add_area2():
-    r = Rectangle(2, 5, 2, 5)  # 10
-    s = Circle(0, 0, 2)  # 12.56
+    r = Rectangle(2, 5, 2, 5)  # площадь прямоугольника 10
+    s = Circle(0, 0, 2)  # площадь окружности 12.56
     assert r.add_area(s) == (10 + 12.56)
 
 
-def test_add_area_negative():
-    with pytest.raises(ValueError):
-        r = Rectangle(2, 5, 2, 5)  # 10
-        c = Circle(0, 0, 2)  # 12.56
-    assert c.add_area(r) == (11 + 12.56)
+# проверка исключений при расчете суммы площадей прямоугольника и окружности
+@pytest.mark.parametrize(("side_a", "side_b", "side_c", "side_d", "area", "perimeter", "expectation"),
+                         {(-4, -6, 4, 6, 24, -20, pytest.raises(ValueError)),  # вызовет исключения
+                          (0, 0, 0, 0, 0, 0, pytest.raises(ValueError)),  # вызовет исключения
+                          (5, 10, 5, 10, 50, 30, does_not_raise())})  # не вызовет исключения
+def test_add_area_negative(side_a: int, side_b: int, side_c: int, side_d: int, area: float, perimeter: float, expectation):
+    with expectation:
+        re = Rectangle(side_a, side_b, side_c, side_d)
+        c = Circle(0, 0, 2)  # площадь окружности 12.56
+        assert c.add_area(re) == float(re.get_area() + c.get_area())
