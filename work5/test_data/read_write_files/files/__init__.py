5e3d3973bf0664a322d7d6461fbfa9163f6cc36f
ZemAleks <xandrzem@yandex.ru>
2024-01-14 19:17:57 +0400

    Добавил контекстный менеджер

--- work5/test_data/read_write_files/files/__init__.py
+++ work5/test_data/read_write_files/files/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 
-def get_pah(file_name):     # функция, которая принимает имя файла
+def get_path(file_name):     # функция, которая принимает имя файла
     work_folder = os.path.dirname(os.path.abspath(__file__))    # функция получает абсолютный путь до директории __file__
     return os.path.join(work_folder, file_name)     # объединяем абсолютный путь с именем папки используя параметр join
 # Оператор join автоматически определяет ОС.
@@ -9,12 +9,12 @@ def get_pah(file_name):     # функция, которая принимает 
 # Библиотека os выбирает необходимые правила указания пути, чтобы получился валидный путь, в рамках ОС на рабочей станции системы.
 
 
-TXT_FILE = get_pah('poem.txt')
-CSV_FILE = get_pah('users.csv')
-JSON_FILE = get_pah('users.json')
-YAML_FILE = get_pah('config.yaml')
+TXT_FILE = get_path('poem.txt')
+CSV_FILE = get_path('users.csv')
+JSON_FILE = get_path('users.json')
+YAML_FILE = get_path('config.yaml')
 
-TXT_FILE_W = get_pah('poem_w.txt')
-CSV_FILE_W = get_pah('users_w.csv')
-JSON_FILE_W = get_pah('users_w.json')
-YAML_FILE_W = get_pah('config_w.yaml')
+TXT_FILE_W = get_path('poem_w.txt')
+CSV_FILE_W = get_path('users_w.csv')
+JSON_FILE_W = get_path('users_w.json')
+YAML_FILE_W = get_path('config_w.yaml')
